a75619ce0e822b74e8b3fb4242b833815a5f15e7
Christian Ramirez <christian.r0602@gmail.com>
2020-11-16 20:58:00 -0600

    Up until handleInputChnage

--- employee-directory/src/App.js
+++ employee-directory/src/App.js
@@ -1,25 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
+import Table from './components/table';
+import Search from './components/search';
+import {Component} from 'react'
+import API from './utils/API'
+class  App extends Component {
+   state ={
+     employees: [],
+     search: ''
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+   }
+
+   componentDidMount(){
+    API.search().then(employees=>{
+      this.setState({
+        employees: employees.data.results
+      })
+    })
+   }
+
+   handleInputChange=event=>{
+     const {name, value} = event.target
+     this.setState({
+        [name]: value
+     })
+
+      console.log(value)
+   }
+
+  render(){
+    return (
+      <div className="App">
+         <Search  keyWord ={this.state.search} handleInputChange={this.handleInputChange} />
+         <Table employees={this.state.employees} />
+      </div>
+    );
+  }
+ 
+} 
 
 export default App;
